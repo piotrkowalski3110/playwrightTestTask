569b43ac88cb9258d5750e2ee742f2db48ebe462
piotrkowalski3110 <piotr.kowalski3110@gmail.com>
2025-11-14 20:55:07 +0100

    auth fix

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -2,17 +2,15 @@ import { test, expect } from '@playwright/test';
 import { tester8 } from '../lib/utils/credentials';
 
 test('Login to website', async ({ page }) => {
-  // go to login page
   await page.goto('https://testing.muras.eu/');
-  await page.getByRole('link', { name: 'Log in' }).click();
 
-  // fill and submit login form
+  await page.getByRole('link', { name: 'Log in' }).click();
+  await expect(page.getByRole('textbox', { name: 'username' })).toBeVisible();
+
   await page.getByRole('textbox', { name: 'username' }).fill(tester8.email);
   await page.getByRole('textbox', { name: 'password' }).fill(tester8.password);
-  await page.getByRole('button', { name: 'Log in' }).click();
-  await page.waitForTimeout(3000);
-  expect(page.url()).toMatch('https://testing.muras.eu/my/');
 
-  // save cookies to file
+  await Promise.all([page.waitForURL('**/my/'), page.getByRole('button', { name: 'Log in' }).click()]);
+
   await page.context().storageState({ path: 'playwright/.auth.json' });
 });
