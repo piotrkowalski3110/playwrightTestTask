bb0d60486aa5d7dadd3ff48a8e4ceffed4343109
piotrkowalski3110 <piotr.kowalski3110@gmail.com>
2025-11-15 02:24:37 +0100

    fix issues

--- tests/testTask.spec.ts
+++ tests/testTask.spec.ts
@@ -2,60 +2,57 @@ import { test, expect, Locator } from '@playwright/test';
 import { fakerPL } from '@faker-js/faker';
 
 test.use({ storageState: 'playwright/.auth.json' });
-const loremWord: string = fakerPL.lorem.word();
-const loremLines: string = fakerPL.lorem.lines();
 
-test('Course management page', async ({ page }) => {
-  await page.goto('https://testing.muras.eu/course/management.php'); // go to management page
-
-  const categoryItems: Locator = page.locator('#course-listing .card-body ul li'); // locate course items
-  const count: number = await categoryItems.count(); // count course items
-  expect(count).toBeGreaterThan(0); // check if there are more than 0 items
-  const randomIndex: number = Math.floor(Math.random() * count); // generate random index
-
-  await categoryItems.nth(randomIndex).locator('.custom-checkbox').click(); // click checkbox
-  await expect(categoryItems.nth(randomIndex).locator('input')).toBeChecked(); // check if checkbox is checked
+test('Course management page - random checkbox', async ({ page }) => {
+  await page.goto('https://testing.muras.eu/course/management.php');
+  const categoryItems: Locator = page.locator('#course-listing .card-body ul li');
+  const count: number = await categoryItems.count();
+  expect(count).toBeGreaterThan(0);
+  const randomIndex: number = Math.floor(Math.random() * count);
+  await categoryItems.nth(randomIndex).locator('.custom-checkbox').click();
+  await expect(categoryItems.nth(randomIndex).locator('input')).toBeChecked();
 });
 
 test.describe.serial('Create + clean up test', () => {
+  const loremWord: string = fakerPL.animal.dog();
+  const loremLines: string = fakerPL.animal.cat();
+
   test('Add activity or resource to course', async ({ page }) => {
-    await page.goto('https://testing.muras.eu/course/view.php?id=7'); // go to course management page
+    await page.goto('https://testing.muras.eu/course/view.php?id=7');
+    await page.waitForURL('**/course/view.php?id=7');
 
-    const checkboxEditMode: Locator = page.getByRole('checkbox', { name: 'Edit mode' }); // get edit mode checkbox
-    await checkboxEditMode.setChecked(true); // set checkbox state to true
-    await expect(checkboxEditMode).toBeChecked(); // check if checkbox is checked
+    const checkboxEditMode: Locator = page.getByRole('checkbox', { name: 'Edit mode' });
+    await checkboxEditMode.setChecked(true);
+    await expect(checkboxEditMode).toBeChecked();
+    await page.getByRole('button', { name: 'Add an activity or resource' }).first().click();
+    await page.getByLabel('Activity modules').getByRole('link', { name: 'Text and media area' }).click();
 
-    await page.getByRole('button', { name: 'Add an activity or resource' }).nth(1).click(); // click second add activity button
-    //await page.waitForTimeout(2000);
-    //await expect(page.locator('.modal-content')).toBeVisible(); // check if modal is visible
-    await page.getByLabel('Activity modules').getByRole('link', { name: 'Text and media area' }).click(); // click text and media area
+    await Promise.all([
+      page.waitForURL('**/course/modedit**'),
+      page.locator('#fitem_id_name input').fill(loremWord),
+      page.getByRole('textbox', { name: 'Text' }).fill(loremLines),
+      page.waitForTimeout(500),
+    ]);
 
-    await page.locator('#fitem_id_name input').fill(loremWord); // fill title with one world
-    await page.getByRole('textbox', { name: 'Text' }).fill(loremLines); // fill text with lorem lines
-    await page.getByRole('button', { name: 'Save and return to course' }).click(); // submit changes
+    await page.getByRole('button', { name: 'Save and return to course' }).click();
 
-    expect(page.url()).toMatch('https://testing.muras.eu/course/view.php?id=7'); // go back to course management page
-    await expect(page.locator(`[data-activityname="${loremWord}"]`)).toBeVisible(); // check if title is created
-    await expect(page.getByText(loremLines)).toBeVisible(); // check if text is visible and created
+    await Promise.all([
+      page.waitForURL('**/course/view.php?id=7'),
+      expect(page.locator('div.no-overflow').getByText(loremWord + loremLines)).toBeVisible(),
+    ]);
   });
 
   test('Clean up created content', async ({ page }) => {
-    await page.goto('https://testing.muras.eu/course/view.php?id=7'); // go to course management page
+    await page.goto('https://testing.muras.eu/course/view.php?id=7');
+    const elementToDelete: Locator = page.locator(`[data-activityname="${loremWord + loremLines}"]`);
+    const optionsMenuButton: Locator = elementToDelete.locator('.activity-actions.bulk-hidden.align-self-start.ms-sm-2');
+    const deleteButton: Locator = optionsMenuButton.getByText('Delete');
+    await optionsMenuButton.click();
+    await deleteButton.click();
 
-    const checkboxEditMode: Locator = page.getByRole('checkbox', { name: 'Edit mode' }); // get edit mode checkbox
-    await checkboxEditMode.setChecked(true); // set checkbox state to true
-    await expect(checkboxEditMode).toBeChecked(); // check if checkbox is checked
-
-    const elementToDelete: Locator = page.locator(`[data-activityname="${loremWord}"]`); // locate previously created activity
-    const optionsMenuButton: Locator = elementToDelete.locator('.activity-actions.bulk-hidden.align-self-start.ms-sm-2'); // locate options menu button
-    const deleteButton: Locator = optionsMenuButton.getByText('Delete'); // locate delete button
-
-    await optionsMenuButton.click(); // click options menu button
-    await deleteButton.click(); // click delete button
-
-    const deleteModal: Locator = page.locator('.modal-dialog'); // locate delete modal
-    //await page.waitForTimeout(2000);
-    //await expect(deleteModal).toBeVisible(); // check if modal is visible
-    await deleteModal.locator('button.btn-danger').click(); // delete content
+    const deleteModal: Locator = page.locator('.modal-dialog');
+    await expect(deleteModal).toBeVisible();
+    await expect(page.getByText(`This will delete ${loremWord + loremLines} and any user data it contains.`)).toBeVisible();
+    await deleteModal.locator('button.btn-danger').click();
   });
 });
